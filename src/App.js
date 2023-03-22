b7a1dc3ea4ae6fe6fde28af05c037bb86a5fb9c8
Ali <dev.alifm@gmail.com>
2023-03-22 19:09:13 -0300

    Warning sin node modules

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./redux/storeConfig";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import AdminHome from "./components/AdminHome";
+import Example from "./components/Cart";
 
 function App() {
   return (
     <PersistGate loading={null} persistor={persistor}>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/prueba" element={<Navbar />} />
+          <Route path="/" element={<Navbar />} />
+          {/* <Route path="/prueba" element={<Navbar />} /> */}
         </Routes>
       </div>
     </PersistGate>
