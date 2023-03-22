94df767e21aebf6f8ad2ee90957ad4f540e3848a
Ali <dev.alifm@gmail.com>
2023-03-22 17:46:13 -0300

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,21 +8,22 @@ function NavbarNav() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Ecozy Luxury</Navbar.Brand>
+        <Navbar.Brand href="/" className="fs-3 tx-color">
+          Ecozy Luxury
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">About this project </Nav.Link>
+            <NavDropdown title="Categories" className="w-25">
+              <NavDropdown.Item href="#action/3.1">Living</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Bedroom</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Dining</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Sets</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Inspiration
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
