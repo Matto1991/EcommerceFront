c016b2ee2561ce517159588cfdab5072a73918be
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2023-03-23 18:52:19 -0300

    Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,15 +28,39 @@ function NavbarNav() {
       expand="lg"
     >
       <Container>
-        <Navbar.Brand href="/" className="fs-3 navbar-logo">
+        <Navbar.Brand
+          href="/"
+          className={`${
+            isScrolling ? "navbar-dynamic-logo" : "navbar-static-logo"
+          }`}
+        >
           Ecozy Luxury
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Our team </Nav.Link>
-            <NavDropdown title="Categories" className="w-25">
+            <Nav.Link
+              href="/"
+              className={`${
+                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+              }`}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#link"
+              className={`${
+                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+              }`}
+            >
+              Our team{" "}
+            </Nav.Link>
+            <NavDropdown
+              className={`w-25 ${
+                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+              }`}
+              title="Categories"
+            >
               <NavDropdown.Item href="#action/3.1">Living</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Bedroom</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Dining</NavDropdown.Item>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const [featured, setFeatured] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     const getFeatured = async () => {
@@ -22,89 +23,169 @@ export default function Home() {
     getFeatured();
   }, []);
 
+  useEffect(() => {
+    const getCategories = async () => {
+      const response = await axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_BACKEND_URL}/categories`,
+      });
+
+      setCategories(response.data);
+
+      console.log(response.data);
+    };
+
+    getCategories();
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="row hero-image">
-        <div className="col-12 position-relative">
-          <div className="hero-text">
-            <h1 className="hero-title mb-3">In soft sculpture</h1>
-            <h4 className="hero-cta">SHOP THE COLLECTION</h4>
+        <div className="col-12">
+          <div className="d-flex justify-content-center align-items-center hero-text h-100">
+            <div className="hero-title-container rounded">
+              <h1 className="hero-title mb-4">In soft sculpture</h1>
+              <div className="d-flex justify-content-end w-100">
+                <h4 className="hero-cta">SHOP THE COLLECTION</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="featured-section container">
-        <div className="d-flex justify-content-center w-100 mb-5">
-          <h2>Featured</h2>
-        </div>
-        <div className="row g-4">
-          {featured.map((product, index) => {
-            return (
-              <>
-               
-                <Link to={`product/${product.id}`} className="col-12 col-md-6 col-lg-3 text-decoration-none text-reset">
-                  <div className="featured-card card d-flex flex-fill h-100">
-                    <img
-                      src={`${process.env.REACT_APP_BACKEND_URL}/${product.images[0]}`}
-                      alt={`${product.name}`}
-                      className="rounded-top"
-                    />
-                    <div className="card-body d-flex flex-column justify-content-between">
-                      <div>
-                        <h5 className="card-title">{product.name}</h5>
-                        <p className="card-text">{product.description}</p>
+      {categories.length > 0 && (
+        <>
+          <div className="featured-section mx-auto">
+            <div className="d-flex justify-content-center w-100 mb-5">
+              <h2>Featured</h2>
+            </div>
+            <div className="row g-4">
+              {featured.map((product) => {
+                return (
+                  <div key={product.id} className="col-12 col-md-6 col-lg-3">
+                    <Link
+                      to={`product/${product.id}`}
+                      className="text-decoration-none text-black"
+                    >
+                      <div className="featured-card card d-flex flex-fill h-100">
+                        <img
+                          src={`${process.env.REACT_APP_BACKEND_URL}/${product.images[0]}`}
+                          alt={`${product.name}`}
+                          className="rounded-top"
+                        />
+                        <div className="card-body d-flex flex-column justify-content-between">
+                          <div>
+                            <h5 className="card-title">{product.name}</h5>
+                            <p className="card-text">{product.description}</p>
+                          </div>
+                          <p className="card-text mt-4">
+                            <small className="bold card-price">
+                              From US$ {`${product.price}`}
+                            </small>
+                          </p>
+                        </div>
                       </div>
-                      <p className="card-text mt-4">
-                        <small className="bold card-price">
-                          US$ {`${product.price}`}
-                        </small>
-                      </p>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-center team-section">
+            <div className="rounded text-center cta">Meet our team</div>
+          </div>
+
+          <div className="categories-section">
+            <div className="d-flex justify-content-center w-100 mb-4">
+              <h2>Shop by categories</h2>
+            </div>
+            <div className="row g-2 mx-auto categories-container h-50 mb-2">
+              <div className="col-8">
+                <div
+                  className="d-flex justify-content-center align-items-center rounded category-card"
+                  style={{
+                    backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL}/${categories[0].image})`,
+                    backgroundRepeat: "no-Repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                >
+                  <h3 className="text-white display-6 category-card-title">
+                    {categories[0].name}
+                  </h3>
+                </div>
+              </div>
+              <div className="col-4">
+                <div
+                  className="d-flex justify-content-center align-items-center rounded category-card"
+                  style={{
+                    backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL}/${categories[1].image})`,
+                    backgroundRepeat: "no-Repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                >
+                  <h3 className="text-white display-6">{categories[1].name}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="row g-2 mx-auto categories-container h-50">
+              <div className="col-4">
+                <div
+                  className="d-flex justify-content-center align-items-center rounded category-card"
+                  style={{
+                    backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL}/${categories[2].image})`,
+                    backgroundRepeat: "no-Repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                >
+                  <h3 className="text-white display-6">{categories[2].name}</h3>
+                </div>
+              </div>
+              <div className="col-8">
+                <div
+                  className="d-flex justify-content-center align-items-center rounded category-card"
+                  style={{
+                    backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL}/${categories[3].image})`,
+                    backgroundRepeat: "no-Repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                >
+                  <h3 className="text-white display-6">{categories[3].name}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="inspiration-section">
+              <div className="row mx-auto inspiration-section-row">
+                <div className="col-4">
+                  <img
+                    src="/img/inspiration.webp"
+                    alt="inspiration"
+                    width="100%"
+                  />
+                </div>
+
+                <div className="col-8">
+                  <div className="d-flex flex-column justify-content-center align-items-center h-100">
+                    <h4 className="fs-2">Get Inspired</h4>
+                    <p className="mb-5 text-secondary">
+                      Discover our favorite looks across the home to inspire
+                      your space.
+                    </p>
+                    <div className="rounded text-center inspiration-cta">
+                      Meet our team
                     </div>
                   </div>
-                  </Link>
-                  
-              </>
-            );
-          })}
-        </div>
-      </div>
-
-      <div className=" d-flex justify-content-center team-section">
-        <div className="rounded text-center cta">Our team</div>
-      </div>
-
-      <div className="categories-section">
-        <div className="d-flex justify-content-center w-100 mb-5">
-          <h2>Shop by categories</h2>
-        </div>
-        <div className="row g-2 mx-auto categories-container h-50 mb-2">
-          <div className="col-8">
-            <div className="d-flex justify-content-center align-items-center rounded category-card living-category">
-              <h3 className="text-white display-5 category-card-title">
-                Living
-              </h3>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-4">
-            <div className="d-flex justify-content-center align-items-center rounded category-card bedroom-category">
-              <h3 className="text-white display-5">Bedroom</h3>
-            </div>
-          </div>
-        </div>
-        <div className="row g-2 mx-auto categories-container h-50">
-          <div className="col-4">
-            <div className="d-flex justify-content-center align-items-center rounded category-card dining-category">
-              <h3 className="text-white display-5 ">Dining</h3>
-            </div>
-          </div>
-          <div className="col-8">
-            <div className="d-flex justify-content-center align-items-center rounded category-card sets-category">
-              <h3 className="text-white display-5">Sets</h3>
-            </div>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
