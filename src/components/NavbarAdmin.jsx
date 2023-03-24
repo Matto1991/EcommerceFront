bf3a3d145a939a11714573c6f3ea0aff8dc6e6e8
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-03-23 22:48:59 -0300

    Navbar de admin

---
+++ src/components/NavbarAdmin.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavbarAdmin() {
+  return (
+    <div className="mt-4">
+      {/* <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav> */}
+    </div>
+  );
+}
+
+export default NavbarAdmin;
