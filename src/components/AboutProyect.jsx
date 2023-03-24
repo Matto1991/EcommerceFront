7de37dfdf24f411f78dbb85c5bcd3d80fd9c1dc4
Ali <dev.alifm@gmail.com>
2023-03-24 12:54:07 -0300

    about update

--- src/components/AboutProyect.jsx
+++ src/components/AboutProyect.jsx
@@ -3,33 +3,119 @@ import Navbar from "../components/Navbar";
 import About from "../components/About.css";
 
 export default function AboutProyect() {
+  {
+    // /* <img src="img/AlineFarias.jpg" className="card-img" alt="ali" /> */
+  }
+
   return (
     <>
       <Navbar />
       <div className="row hero-about">
-        <div className="col-12 position-relative">
-          <div className="hero-text"></div>
-        </div>
+        <div className="col-12 position-relative"></div>
       </div>
 
-      <div className="container">
-        <div className="col-4 m-3">
-          <div class="card">
-            <div class="card-body">
-              <img src="img/AlineFarias.jpg" className="card-img" alt="ali" />
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <p>REDES</p>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-3 col-sm-6 col-md-4 col-lg-3 p-3  mt-5 mb-5">
+            <div class="card">
+              <div className="rounded small-box"></div>
+              <div class="card-body m-3">
+                <div className="icon"> </div>
+                <h5 class="card-title">Duración</h5>
+                <p class="card-text">
+                  El proyecto fue desarrollado en tan sólo 3 semanas, durante
+                  noviembre de 2022. El mismo se dividió en sprints (Scrum) de
+                  una semana de duración.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="col-4 border border-3">
-            <img src="img/AlineFarias.jpg" className="w-100 team" alt="ali" />
-            <div className="d-flex "></div>
-            <h3>Aline Farias</h3>
-            <h5>Full Stack Developer Jr</h5>
+          <div className="col-3 col-sm-6 col-md-4 col-lg-3 p-3  mt-5 mb-5">
+            <div class="card">
+              <div className="rounded small-box"></div>
+              <div class="card-body m-3">
+                <div className="icon"> </div>
+                <h5 class="card-title">Tecnologías</h5>
+                <p class="card-text">
+                  Para el Front-End del sitio se desarrolló una aplicación en
+                  React (usando Create-React-App) mientras que para el Back-End
+                  se desarrolló una REST API hecha con Node.js, Express, SQL y
+                  Git/GitHub
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-3 col-sm-6 col-md-4 col-lg-3 p-3  mt-5 mb-5">
+            <div class="card">
+              <div className="rounded small-box"></div>
+              <div class="card-body m-3">
+                <div className="icon"> </div>
+                <h5 class="card-title">División de tareas</h5>
+                <p class="card-text">
+                  Para la organización de tareas durante el proyecto se utilizó
+                  Trello y Figma. Esto permitió que cada integrante del equipo
+                  estuviese permanentemente al tanto del estado del proyecto así
+                  como de las tareas que debía realizar.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row mt-5 d-flex justify-content-evenly">
+          <div className="col-6 col-sm-6 col-md-4 col-lg-3 p-3 shadow card-container rounded mt-5 mb-5">
+            <div className="shape-team">
+              <div className="img-team-andy"></div>
+            </div>
+            <h3 className="mt-3 fw-bold">Andrés Viera</h3>
+            <h4 className="card-team-title"> Full Stack Developer Jr </h4>
+            <br />
+            <div className="icons-team">
+              <i class="bi bi-linkedin me-2"></i>
+              <i class="bi bi-envelope-fill me-2"></i>
+              <i class="bi bi-github"></i>
+            </div>
+          </div>
+          <div className="col-6 col-sm-6 col-md-4 col-lg-3 p-3 shadow card-container rounded mt-5 mb-5">
+            <div className="shape-team">
+              <div className="img-team-vir"></div>
+            </div>
+            <h3 className="mt-3 fw-bold">Virginia Barbaro</h3>
+            <h4 className="card-team-title"> Full Stack Developer Jr </h4>
+            <br />
+            <div className="icons-team">
+              <i class="bi bi-linkedin me-2"></i>
+              <i class="bi bi-envelope-fill me-2"></i>
+              <i class="bi bi-github"></i>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3 d-flex justify-content-evenly">
+          <div className="col-6 col-sm-6 col-md-4 col-lg-3 p-3 shadow card-container rounded mt-5 mb-5">
+            <div className="shape-team">
+              <div className="img-team-ali"></div>
+            </div>
+            <h3 className="mt-3 fw-bold">Aline Farias</h3>
+            <h4 className="card-team-title"> Full Stack Developer Jr </h4>
+            <br />
+            <div className="icons-team">
+              <i class="bi bi-linkedin me-2"></i>
+              <i class="bi bi-envelope-fill me-2"></i>
+              <i class="bi bi-github"></i>
+            </div>
+          </div>
+          <div className="col-6 col-sm-6 col-md-4 col-lg-3 p-3 shadow card-container rounded mt-5 mb-5">
+            <div className="shape-team">
+              <div className="img-team-andy"></div>
+            </div>
+            <h3 className="mt-3 fw-bold">Matias Sandín</h3>
+            <h4 className="card-team-title"> Full Stack Developer Jr </h4>
+            <br />
+            <div className="icons-team">
+              <i class="bi bi-linkedin me-2"></i>
+              <i class="bi bi-envelope-fill me-2"></i>
+              <i class="bi bi-github"></i>
+            </div>
           </div>
         </div>
       </div>
