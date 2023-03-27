2041715faf3cb486484ba4e64b00b4e2950c00ac
Ali <dev.alifm@gmail.com>
2023-03-27 02:41:16 -0300

    up date about us

--- src/components/AboutProyect.jsx
+++ src/components/AboutProyect.jsx
@@ -18,7 +18,7 @@ export default function AboutProyect() {
     <span className="p-2 rounded icon-about"><i class="fs-5 bi bi-clock text-white"></i></span>
       <div className="card-body mt-3">
         <h5 className="card-title">Duración</h5>
-        <p className="card-text">El proyecto fue desarrollado en tan sólo 3 semanas, durante marzo de 2023. El mismo se dividió en sprints (Scrum) de una semana de duración.</p>
+        <p className="card-text">El proyecto fue desarrollado en tan sólo 3 semanas, durante marzo de 2023. El mismo se dividió en demos de una semana de duración.</p>
       </div>
     </div>
   </div>
@@ -28,7 +28,7 @@ export default function AboutProyect() {
     <span className="p-2 rounded icon-about"><i class="fs-5 bi bi-code-square text-white"></i></span>
       <div className="card-body mt-3">
         <h5 className="card-title">Tecnologías</h5>
-        <p className="card-text">Para el Front-End del sitio se desarrolló una aplicación en React (usando Create-React-App) mientras que para el Back-End se desarrolló una REST API hecha con Node.js, Express, SQL y Git/GitHub.</p>
+        <p className="card-text">Para el Front-End del sitio se desarrolló una aplicación en React, mientras que para el Back-End se desarrolló una REST API hecha con Node.js, Express, mySQL y Git/GitHub.</p>
       </div>
     </div>
   </div>
@@ -43,13 +43,89 @@ export default function AboutProyect() {
     </div>
   </div>
 </div>
+<hr />
 
-<div>
-  <h2>Equipo:</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta inventore eius obcaecati optio nemo officia minima aliquam vero ratione illum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, inventore?</p>
+ 
+<div className="mx-auto mt-5">
+  <h1 className="text-center mb-5 fw-bold">Equipo</h1>
+<div class="row g-2">
+  <div class="col-md-6  col-lg-3">
+    <div class="card border-0 p-3">
+      <img src="/img/team_Andres.png" class="img-team-ali shadow mx-auto" alt="..."/>
+      <div class="card-body text-center">
+        <h5 class="card-title">Andrés Viera</h5>
+        <p class="card-text fw-bold ">Full Stack Developer Jr.</p>
+        
+      </div>
+      
+                <div className="icons-team text-center">
+                  <i className="bi bi-linkedin me-2"></i>
+                  <i className="bi bi-github me-2"></i>
+                  <i className="bi bi-envelope-fill "></i>
+                </div>
+    </div>
+  </div>
+  <div class="col-md-6 col-lg-3">
+    <div class="card border-0 p-3">
+      <img src="/img/VirginiaBarbaroimg.jpg" class="img-team-ali shadow mx-auto" alt="..."/>
+      <div class="card-body text-center">
+        <h5 class="card-title">Virginia Barbaro</h5>
+        <p class="card-text fw-bold ">Full Stack Developer Jr.</p>
+        
+      </div>
+      
+                <div className="icons-team text-center">
+                  <i className="bi bi-linkedin me-2"></i>
+                  <i className="bi bi-github me-2"></i>
+                  <i className="bi bi-envelope-fill "></i>
+                </div>
+    </div>
+  </div>
+  <div class="col-md-6 col-lg-3">
+    <div class="card border-0 p-3">
+      <img src="/img/AlineFariasprueba.jpg" class="img-team-ali shadow mx-auto" alt="..."/>
+      <div class="card-body text-center">
+        <h5 class="card-title">Aline Farias</h5>
+        <p class="card-text fw-bold ">Full Stack Developer Jr.</p>
+        
+      </div>
+      
+                <div className="icons-team text-center">
+                  <i className="bi bi-linkedin me-2"></i>
+                  <i className="bi bi-github me-2"></i>
+                  <i className="bi bi-envelope-fill "></i>
+                </div>
+    </div>
+  </div>
+  <div class="col-md-6 col-lg-3">
+    <div class="card border-0 p-3">
+      <img src="/img/Mathi.jpg" class="img-team-ali shadow mx-auto" alt="..."/>
+      <div class="card-body text-center">
+        <h5 class="card-title">Mathias Sandin</h5>
+        <p class="card-text fw-bold ">Full Stack Developer Jr.</p>
+        
+      </div>
+      
+                <div className="icons-team text-center">
+                  <i className="bi bi-linkedin me-2"></i>
+                  <i className="bi bi-github me-2"></i>
+                  <i className="bi bi-envelope-fill "></i>
+                </div>
+    </div>
+  </div>
+ 
 </div>
+          </div>
+          
+        
+      </div>
+      <Footer />
+    </>
+  );
+}
 
-          <div className="row text-center">
+
+<div className="row text-center">
             <div className="col-12 col-md-6  col-xl-3 p-3  ">
               <div className="shadow card-container rounded">
                 <div className="shape-team">
@@ -111,9 +187,3 @@ export default function AboutProyect() {
               </div>
             </div>
           </div>
-        
-      </div>
-      <Footer />
-    </>
-  );
-}
