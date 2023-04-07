8dc5860dcf1dce9b7b9c5e6b8b1be554bfafa344
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-04-07 12:09:44 -0300

    btn de sobre project responsive in movil,  un poco mas de luminosidad al hacer hover a las categories

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -47,75 +47,77 @@ function NavbarHome() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav id="navbar-movil">
-            <div
-              className={`position-relative ${
-                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
-              }`}
-            >
-              <Nav.Link href="/">Home</Nav.Link>
-            </div>
-            <div
-              className={`position-relative ${
-                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
-              }`}
-            >
-              <Nav.Link href="/products">Products</Nav.Link>
-            </div>
+            <Nav id="navbar-movil">
+              <div
+                className={`position-relative ${
+                  isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+                }`}
+              >
+                <Nav.Link href="/">Home</Nav.Link>
+              </div>
+              <div
+                className={`position-relative ${
+                  isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+                }`}
+              >
+                <Nav.Link href="/products">Products</Nav.Link>
+              </div>
 
-            <div
-              className={`position-relative ${
-                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
-              }`}
-            >
-              <Nav.Link href="/project/details">About&nbsp;Project</Nav.Link>
-            </div>
+              <div
+                className={`position-relative ${
+                  isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+                }`}
+              >
+                <Nav.Link href="/project/details">About&nbsp;Project</Nav.Link>
+              </div>
 
-            <NavDropdown
-              className={`w-25 ${
-                isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
-              }`}
-              title="Categories"
-            >
-              <NavDropdown.Item href="/category/living">
-                Living
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/category/bedroom">
-                Bedroom
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/category/dining">
-                Dining
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/category/sets">Sets</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Inspiration
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Avatar
-              src={
-                loggedUser
-                  ? `${process.env.REACT_APP_BACKEND_URL}/img/users/${loggedUser.avatar}`
-                  : `${process.env.REACT_APP_BACKEND_URL}/img/users/no_user.png`
-              }
-              alt={loggedUser ? `${loggedUser.firstname}` : "User picture"}
-              className="navbar-avatar ms-4"
-            />
-            <NavDropdown
-              className={` ${
-                isScrolling
-                  ? "navbar-dynamic-dropdown"
-                  : "navbar-static-dropdown"
-              }`}
-            >
-              <NavDropdown.Item href="/orders">Orders</NavDropdown.Item>
-              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-              <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link className="d-flex">
-              <Cart />
-            </Nav.Link>
-          </Nav>
+              <NavDropdown
+                className={`w-25 ${
+                  isScrolling ? "navbar-dynamic-text" : "navbar-static-text"
+                }`}
+                title="Categories"
+              >
+                <NavDropdown.Item href="/category/living">
+                  Living
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/category/bedroom">
+                  Bedroom
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/category/dining">
+                  Dining
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/category/sets">Sets</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Inspiration
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Avatar
+                src={
+                  loggedUser
+                    ? `${process.env.REACT_APP_BACKEND_URL}/img/users/${loggedUser.avatar}`
+                    : `${process.env.REACT_APP_BACKEND_URL}/img/users/no_user.png`
+                }
+                alt={loggedUser ? `${loggedUser.firstname}` : "User picture"}
+                className="navbar-avatar ms-4"
+              />
+              <NavDropdown
+                className={` ${
+                  isScrolling
+                    ? "navbar-dynamic-dropdown"
+                    : "navbar-static-dropdown"
+                }`}
+              >
+                <NavDropdown.Item href="/orders">Orders</NavDropdown.Item>
+                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                <NavDropdown.Item onClick={handleLogout}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link className="d-flex">
+                <Cart />
+              </Nav.Link>
+            </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
